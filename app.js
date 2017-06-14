5fab470b7a853f3ebfda7c797a56444613349387
joshcoales <joshua@coales.co.uk>
2017-06-15 00:09:31 +0100

    more jslint on app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+"use strict";
 var express = require("express");
 var path = require("path");
-var favicon = require("serve-favicon");
+//var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
@@ -21,7 +22,7 @@ app.set("view engine", "jade");
 app.use(cors());
 app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -30,21 +31,23 @@ app.use("/categories", categories);
 app.use("/tasks", tasks);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development"
+        ? err
+        : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
